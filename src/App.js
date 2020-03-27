8ddebabe7465111868fe16864cd637ee8b5a3aec
Prem Kumar <prmsrswt@gmail.com>
2020-03-27 12:18:42 +0530

    basic login flow done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import config from "./config";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [profile, setProfile] = useState();
+  const { googleClientId } = config;
+
+  const handleGoogleLogin = googleUser => {
+    setProfile(googleUser.profileObj);
+  };
+
+  const handleFailure = res => {
+    console.log(res);
+  };
+
+  const handleLogout = () => {
+    setProfile(null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {profile ? (
+          <>
+            <p>
+              <code>Welcome to Hell, {profile.givenName}</code>.
+            </p>
+            <GoogleLogout clientId={googleClientId} onLogoutSuccess={handleLogout} theme="dark" />
+          </>
+        ) : (
+          <GoogleLogin
+            clientId={googleClientId}
+            theme="dark"
+            onSuccess={handleGoogleLogin}
+            onFailure={handleFailure}
+            isSignedIn={true}
+          />
+        )}
       </header>
     </div>
   );
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  googleClientId: '866745933273-vqs2o0ipcbvj9l6e4q369b17tetnbvu1.apps.googleusercontent.com',
+};
